380bf0666337599b862fd5d54409598730de1667
Feliipee013 <ftaveira644@icoud.com>
2025-05-16 16:05:44 -0300

    finalizei o service, controller e  dto dos cliente

--- src/main/java/br/com/RollTickets/api/service/ClienteService.java
+++ src/main/java/br/com/RollTickets/api/service/ClienteService.java
@@ -1,10 +1,13 @@
 package br.com.RollTickets.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.RollTickets.api.controller.ClienteCreateDTO;
+import br.com.RollTickets.api.dto.ClienteCreateDTO;
 import br.com.RollTickets.api.dto.ClienteResponseDTO;
+import br.com.RollTickets.api.dto.ClienteUpdateDTO;
 import br.com.RollTickets.api.entity.Cliente;
 import br.com.RollTickets.api.mapper.ClienteMapper;
 import br.com.RollTickets.api.repository.ClienteRepository;
@@ -19,5 +22,30 @@ public class ClienteService {
 		Cliente cliente = ClienteMapper.toEntity(clienteCreateDTO);
 		clienteRepository.save(cliente);
 		return ClienteMapper.toDTO(cliente);
+	}
+	
+	public List<ClienteResponseDTO> list() {
+		return clienteRepository.findAll().stream().map(ClienteMapper::toDTO).toList();
+	}
+	
+	public ClienteResponseDTO show(long id) {
+			Cliente cliente = clienteRepository.findById(id)
+					.orElseThrow(()->new RuntimeException("Cliente com id" + id + " não encontrado"));
+			return ClienteMapper.toDTO(cliente);
+	}
+	
+	public ClienteResponseDTO update(ClienteUpdateDTO clienteUpdateDTO) {
+		Cliente cliente = clienteRepository.findById(clienteUpdateDTO.id()).orElseThrow(()->new RuntimeException("Cliente não encontrado para alteração"));
+		cliente.setCpf(clienteUpdateDTO.cpf());
+		cliente.setEmail(clienteUpdateDTO.email());
+		cliente.setNome(clienteUpdateDTO.nome());
+		cliente.setSenha(clienteUpdateDTO.senha());
+		cliente.setTelefone(clienteUpdateDTO.telefone());
+		return ClienteMapper.toDTO(clienteRepository.save(cliente));
+	}
+	
+	public void destroy(long id) {
+		Cliente cliente= clienteRepository.findById(id).orElseThrow(()->new RuntimeException("Cliente não encontrado para deleção"));
+		clienteRepository.delete(cliente);
 	}
 }
