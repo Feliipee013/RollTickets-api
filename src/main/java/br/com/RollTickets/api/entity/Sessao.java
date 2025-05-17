ec943278de566b8755d87812b9d667e889d082b4
JF532 <jfilipe532@hotmail.com>
2025-05-17 01:59:42 -0300

    Adicionei Sessao

---
+++ src/main/java/br/com/RollTickets/api/entity/Sessao.java
@@ -0,0 +1,83 @@
+package br.com.RollTickets.api.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sessoes")
+public class Sessao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "id_filme")
+    private Filme filme;
+
+    @OneToOne
+    @JoinColumn(name = "id_sala")
+    private Sala sala;
+
+    LocalDateTime horario;
+
+    int quantidade_ingressos_disponiveis;
+
+    public Sessao() {
+    }
+
+    public Sessao(Filme filme, Sala sala, LocalDateTime horario, int quantidade_ingressos_disponiveis) {
+        this.filme = filme;
+        this.sala = sala;
+        this.horario = horario;
+        this.quantidade_ingressos_disponiveis = quantidade_ingressos_disponiveis;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public LocalDateTime getHorario() {
+        return horario;
+    }
+
+    public void setHorario(LocalDateTime horario) {
+        this.horario = horario;
+    }
+
+    public int getQuantidade_ingressos_disponiveis() {
+        return quantidade_ingressos_disponiveis;
+    }
+
+    public void setQuantidade_ingressos_disponiveis(int quantidade_ingressos_disponiveis) {
+        this.quantidade_ingressos_disponiveis = quantidade_ingressos_disponiveis;
+    }
+
+}
