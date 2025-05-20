c9d1dd9a36a0e8e6e0ec90633910ef71fc281a62
Feliipee013 <ftaveira644@icloud.com>
2025-05-20 18:31:57 -0300

    fiz as tabelas sql  do migration

---
+++ src/main/resources/db/migration/V8__create_ingressos3d_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ingressos3d (
+    id BIGINT PRIMARY KEY,
+    inclui_oculos BOOLEAN NOT NULL DEFAULT TRUE,
+    taxa_extra_3d DOUBLE NOT NULL DEFAULT 5.0,
+    FOREIGN KEY (id) REFERENCES ingressos(id)
+);
