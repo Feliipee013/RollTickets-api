5dbfd4970d2454e360fc4e6c7b4a94c3ad0e9d84
Feliipee013 <ftaveira644@icloud.com>
2025-06-03 17:08:27 -0300

    consumo da api para que os filmes em cartaz sejam salvo no banco de dados

--- src/main/java/br/com/RollTickets/api/mapper/FilmeMapper.java
+++ src/main/java/br/com/RollTickets/api/mapper/FilmeMapper.java
@@ -4,22 +4,26 @@ import br.com.RollTickets.api.dto.FilmeResponseDTO;
 import br.com.RollTickets.api.entity.Filme;
 
 public class FilmeMapper {
-    public static FilmeResponseDTO toDTO(Filme filme) {
-		FilmeResponseDTO filmeResponse = new FilmeResponseDTO(filme.getId(), filme.getTitulo(), filme.getSinopse(), filme.getDuracao(), filme.getClassificacao(), filme.getGenero(), filme.getImageUrl(), filme.getFormato());
-		return filmeResponse;
-	}
-	
-	public static Filme toEntity(FilmeCreateDTO filmeCreateDTO) {
-		Filme filme = new Filme();
-		filme.setTitulo(filmeCreateDTO.titulo());
-		filme.setSinopse(filmeCreateDTO.sinopse());
-		filme.setDuracao(filmeCreateDTO.duracao());
-		filme.setClassificacao(filmeCreateDTO.classificacao());
-		filme.setGenero(filmeCreateDTO.genero());
-		filme.setImageUrl(filmeCreateDTO.imageUrl());
-		filme.setFormato(filmeCreateDTO.formato()); 
+	 public static FilmeResponseDTO toDTO(Filme filme) {
+	        return new FilmeResponseDTO(
+	            filme.getId(),
+	            filme.getTitulo(),
+	            filme.getSinopse(),
+	            filme.getDuracao(),
+	            filme.getClassificacao(),
+	            filme.getImageUrl(),
+	            filme.getFormato()
+	        );
+	    }
 
-
-		return filme;
-	}
+	    public static Filme toEntity(FilmeCreateDTO dto) {
+	        return new Filme(
+	            dto.titulo(),
+	            dto.sinopse(),
+	            dto.duracao(),
+	            dto.classificacao(),
+	            dto.imageUrl(),
+	            dto.formato()
+	        );
+	    }
 }
