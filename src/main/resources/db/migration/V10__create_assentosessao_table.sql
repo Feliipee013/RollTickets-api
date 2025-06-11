dcb9f91e290b8bf15dde44740463ecaf33561b07
JF532 <jfilipe532@hotmail.com>
2025-06-11 15:50:20 -0300

    Configurações Finais do ASSENTO/SESSAO

--- src/main/resources/db/migration/V10__create_assentosessao_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE assentos_sessao(
-        id BIGINT PRIMARY KEY AUTO_INCREMENT,
-        assento_id BIGINT,
-        FOREIGN KEY (assento_id) REFERENCES assentos(id),
-        sessao_id BIGINT,
-        FOREIGN KEY (sessao_id) REFERENCES sessoes(id),
-        reservado boolean
-
-);
