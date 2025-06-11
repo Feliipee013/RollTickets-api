7ae1b855275f7a0268b409548ef955cf55a979d4
Feliipee013 <ftaveira644@icloud.com>
2025-06-11 14:59:07 -0300

    Fiz um método para buscar todas as sesoes de um filme

--- src/main/java/br/com/RollTickets/api/repository/SessaoRepository.java
+++ src/main/java/br/com/RollTickets/api/repository/SessaoRepository.java
@@ -3,10 +3,12 @@ import org.springframework.stereotype.Repository;
 
 import br.com.RollTickets.api.entity.Sessao;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface SessaoRepository extends JpaRepository<Sessao, Long>{
-
-    
+	List<Sessao> findByFilmeId(Long filmeId);
 }
