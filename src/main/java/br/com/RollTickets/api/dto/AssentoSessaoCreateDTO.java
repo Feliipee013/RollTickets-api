0adb63d66f1aa45e088242f57e664979113a3a65
JF532 <jfilipe532@hotmail.com>
2025-06-11 15:50:20 -0300

    Configurações Finais do ASSENTO/SESSAO

--- src/main/java/br/com/RollTickets/api/dto/AssentoSessaoCreateDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.RollTickets.api.dto;
-
-
-public record AssentoSessaoCreateDTO(
-    Long assento, //Tive que trocar porque era mais simples só pegar um id do que o objeto inteiro
-    Long sessao//Tive que trocar porque era mais simples só pegar um id do que o objeto inteiro
-) {}
--- src/main/resources/db/migration/V4__create_assentos_table.sql
+++ src/main/resources/db/migration/V4__create_assentos_table.sql
@@ -3,5 +3,7 @@ CREATE TABLE assentos(
     numero INT NOT NULL,
     fileira VARCHAR(10) NOT NULL,
     sala_id BIGINT,
-    FOREIGN KEY (sala_id) REFERENCES salas(id)
+    sessao_id BIGINT,
+    FOREIGN KEY (sala_id) REFERENCES salas(id),
+    FOREIGN KEY (sessao_id) REFERENCES sessoes(id)
 );
