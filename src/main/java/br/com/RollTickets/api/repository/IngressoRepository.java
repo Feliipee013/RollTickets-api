cd66127bac0c85f746214ac772350a23d0f4146c
JF532 <jfilipe532@hotmail.com>
2025-06-15 20:09:41 -0300

    Modificação nas entidades, mappers, etc...

--- src/main/java/br/com/RollTickets/api/repository/IngressoRepository.java
+++ src/main/java/br/com/RollTickets/api/repository/IngressoRepository.java
@@ -2,10 +2,13 @@ package br.com.RollTickets.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
+import br.com.RollTickets.api.entity.Assento;
 import br.com.RollTickets.api.entity.Ingresso;
+import br.com.RollTickets.api.entity.Sessao;
 
 @Repository
-public interface IngressoRepository extends JpaRepository<Ingresso, Long>{
-
+public interface IngressoRepository extends JpaRepository<Ingresso, Long> {
+    boolean existsBySessaoAndAssento(Sessao sessao, Assento assento); //Função para verificar se existe uma sessão e um assento
+    List<Ingresso> findByClienteId(Long clienteId); //Função para achar os ingressos por um id do cliente
 }
