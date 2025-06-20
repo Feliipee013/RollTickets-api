369b91960d5994a499578668f92e71721229d8ba
JF532 <jfilipe532@hotmail.com>
2025-06-20 01:11:07 -0300

    Configurações para a compra do MercadoPago funcionar

--- src/main/java/br/com/RollTickets/api/dto/PagamentoCartaoDTO.java
+++ src/main/java/br/com/RollTickets/api/dto/PagamentoCartaoDTO.java
@@ -1,10 +1,26 @@
 package br.com.RollTickets.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record PagamentoCartaoDTO(
     String token,
+
+    @JsonProperty("payment_method_id")
     String paymentMethodId,
+
+    @JsonProperty("issuer_id")
     String issuerId,
-    String email,
+
+    Payer payer,
+
     Integer installments,
+
+    @JsonProperty("transaction_amount")
     Double valor
-) {}
+) {
+
+    public record Payer(
+        String email
+        // pode adicionar identification se quiser
+    ) {}
+}
